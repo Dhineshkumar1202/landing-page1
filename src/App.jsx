a5a7c96a1afbc57a85c879f830cadf346f09037e
Dhineshkumar1202 <dhineshtrendy12@gmail.com>
2024-07-17 22:21:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Header from './components/Header'
+import Feature from './components/Feature'
+import Showcase from './components/showcase'
+import Testimonial from './components/Testimonial'
+import CTA from './components/CTA'
+import Footer from './components/Footer'
+
+
+
+
+
+
+function App() {
+  return <>
+  <Navbar/>
+  <Header/>
+  <Feature/>
+  <Showcase/>
+  <Testimonial/>
+  <CTA/>
+  <Footer/>
+</>
+}
+
+export default App
